7f04e3015f409ccc98ba0f555a28c1d253fa0937
Taylor Gorman <me@taylorpatrickgorman.com>
2020-04-21 23:21:38 -0500

    officially can’t use themoviedb-javascript-library
    since they use callbacks, i can’t access variables outside the scope of that callback. using my own async await functions will allow more control inline.

--- src/context.js
+++ src/context.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useEffect } from 'react'
 import theMovieDb from 'themoviedb-javascript-library'
 
+import { doFetch } from "./utilities/hooks"
 
 const Context = createContext()
 export default Context
@@ -12,10 +13,11 @@ export function Provider( props ) {
   const [isLoading, setIsLoading] = useState(false)
   const [configuration, setConfiguration] = useState()
   const [discoverOptions, setDiscoverOptions] = useState({})
-  const [movies, setMovies] = useState()
+  const [movies, setMovies] = useState([])
   const [errorMessage, setErrorMessage] = useState()
 
-  // On load
+  // Configuration
+  // pretty much only used for images
   useEffect( ()=>{
 
     setIsLoading( true )
@@ -37,64 +39,70 @@ export function Provider( props ) {
 
   // Discover
   useEffect( ()=>{
+  async function functionName(){
+    console.log('discover')
 
     setIsLoading( true )
 
     const today = new Date()
     const todayFormatted = today.getFullYear() + "-" + String(today.getMonth() + 1).padStart(2,"0") + "-" + String(today.getDate()).padStart(2,"0")
+    let page = 1
+    let totalPages = 9
+    let movieFetches = []
 
-    theMovieDb.discover.getMovies(
-      {
-        ...discoverOptions,
-        region: 'US',
-        language: 'en-US',
-        with_original_language: 'en',
-        include_adult: false,
-        include_video: false,
-        "with_runtime.gte": 60,
-        'primary_release_date.lte': todayFormatted,
-        primary_release_year: 2020,
-      },
-      response => {
-        setMovies( JSON.parse( response ).results )
-        setIsLoading( false )
-      },
-      error => {
-        console.error( error )
-        const json = JSON.parse( error )
-        setErrorMessage( 'api.themoviedb.org ERROR ' + json.status_code + ': ' + json.status_message )
-        setIsLoading( false )
-      }
-    )
+    while ( page < Math.min(totalPages,10) ) {
 
+      const [res,err] = await doFetch( "https://api.themoviedb.org/3/discover/movie", "GET", {
+        api_key: process.env.REACT_APP_TMDB_API_KEY,
+          ...discoverOptions,
+          region: 'US',
+          language: 'en-US',
+          with_original_language: 'en',
+          include_adult: false,
+          include_video: false,
+          "with_runtime.gte": 60,
+          'primary_release_date.lte': todayFormatted,
+          primary_release_year: 2020,
+          page
+      } )
+      if ( err ) break
+      movieFetches.push(...res.results)
+      totalPages = res.total_pages
+
+      page++
+
+    }
+    setMovies(movieFetches)
+
+  }functionName()
   }, [discoverOptions] )
 
   // Credits
-  useEffect( ()=>{
-
-    if ( ! movies ) return
-
-    setIsLoading( true )
-
-    movies.map( movie => {
-      theMovieDb.movies.getCredits(
-        {
-          id: movie.id
-        },
-        response => {
-          const json = JSON.parse( response )
-          setIsLoading( false )
-        },
-        error => {
-          console.error( error )
-          const json = JSON.parse( error )
-          setErrorMessage( 'api.themoviedb.org ERROR ' + json.status_code + ': ' + json.status_message )
-          setIsLoading( false )
-        }
-      )
-    } )
-
-  }, [movies] )
+  //useEffect( ()=>{
+  //
+  //  if ( ! movies ) return
+  //
+  //  setIsLoading( true )
+  //
+  //  movies.map( movie => {
+  //    theMovieDb.movies.getCredits(
+  //      {
+  //        id: movie.id
+  //      },
+  //      response => {
+  //        const json = JSON.parse( response )
+  //        setIsLoading( false )
+  //      },
+  //      error => {
+  //        console.error( error )
+  //        const json = JSON.parse( error )
+  //        setErrorMessage( 'api.themoviedb.org ERROR ' + json.status_code + ': ' + json.status_message )
+  //        setIsLoading( false )
+  //      }
+  //    )
+  //  } )
+  //
+  //}, [movies] )
 
   return (
 
---
+++ src/utilities/hooks.js
@@ -0,0 +1,11 @@
+
+export function doFetch ( endpoint, method, data ) {
+  let body
+  if ( method === "GET" )
+    endpoint = endpoint + "?" + Object.keys(data).map(key => key + '=' + data[key]).join('&')
+  else
+    body = JSON.stringify(data)
+  return fetch( endpoint, { method, body } )
+    .then( async resp => [await resp.json(), null] )
+    .catch( error => [null, error] )
+}
